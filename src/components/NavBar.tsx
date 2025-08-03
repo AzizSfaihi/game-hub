1a8f226cd79d7d1158d1ce450b89b5015a95087a
aziz sfaihi <azizsfaihi10@gmail.com>
2025-08-03 02:09:56 +0100

    Update logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,21 @@
-import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+import { HStack, Image, useColorModeValue } from "@chakra-ui/react";
+import darkLogo from "../assets/dragon-solid-full.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import lightLogo from "../assets/dragon-solid-light.svg";
 
 interface Props {
-  onSearch : (searchText : string) => void,
+  onSearch: (searchText: string) => void;
 }
 
-const NavBar = ({onSearch}:Props) => {
+const NavBar = ({ onSearch }: Props) => {
+  const logoSrc = useColorModeValue(darkLogo, lightLogo);
+
   return (
     <div>
       <HStack padding="10px">
-        <Image src={logo} boxSize="60px" />
-        <SearchInput onSearch={onSearch}/>
+        <Image src={logoSrc} alt="Logo" height="55px" />
+        <SearchInput onSearch={onSearch} />
         <ColorModeSwitch />
       </HStack>
     </div>
